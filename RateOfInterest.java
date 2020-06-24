2e4ac208af32cd550376ff32ae188228c344df58
Draksha22 <drakshakhan2211@gmail.com>
2020-06-24 14:05:44 +0530

    this is a simple java program

---
+++ RateOfInterest.java
@@ -0,0 +1,13 @@
+
+package apnikaksha;
+
+public class RateOfInterest {
+    
+      public static void main(String[] args) {
+      int age = 10;
+      float rateOfInterest = 2.3f;
+      double rate = 3.14;
+      System.out.println(rateOfInterest);
+    }
+    
+}
