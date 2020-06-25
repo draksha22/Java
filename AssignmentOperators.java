35ced95ea0f8381bf04ed29316e40b53eea534e8
Draksha22 <drakshakhan2211@gmail.com>
2020-06-25 12:11:13 +0530

    this program shows assignment operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,14 @@
+
+package apnikaksha;
+
+public class AssignmentOperators {
+    
+    public static void main(String[] args){
+        
+        int a = 4;
+         a <<= 2;
+        
+         System.out.println(a);
+        
+    }
+}
