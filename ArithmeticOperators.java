dedf2f2ecd911ac217f4b5b4cc760ca0d86800a7
Draksha22 <drakshakhan2211@gmail.com>
2020-06-25 12:08:30 +0530

    this program show different arithmetic operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,28 @@
+
+package apnikaksha;
+
+public class ArithmeticOperators {
+    
+    public static void main(String[] args){
+        
+        int a = 23;
+        int b = 45;
+        
+        int c = a + b;
+        
+        int d = b - a;
+        
+        int x = b / a;
+        
+        int y = b % a;
+        
+        int z = b++;
+        
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(b);
+    }
+}
