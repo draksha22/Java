c99d1255a57874a8f292759a2f7551e59ea60514
Draksha22 <drakshakhan2211@gmail.com>
2020-06-26 21:36:59 +0530

    this programs contain different bitwise operators

---
+++ BitwiseOperators.java
@@ -0,0 +1,21 @@
+
+package apnikaksha;
+
+public class BitwiseOperators {
+    
+   public static void main (String[] args){
+       
+       int a = 5;
+       int b = 6;
+       int c = a & b;
+       int d = a | b;
+       int x = a ^ b;
+       
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(x);
+    
+       
+   } 
+    
+}
