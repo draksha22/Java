8e477a0d93ee46f2f86974ff86833a21bdac291a
Draksha22 <drakshakhan2211@gmail.com>
2020-06-27 20:50:19 +0530

    this program prints number from 1 to 100 but it will break as soon as the no becomes equal to 35

---
+++ Break1.java
@@ -0,0 +1,21 @@
+
+package apnikaksha;
+
+public class Break1 {
+    
+    public static void main(String[] args){
+        
+        for(int i = 1; i <= 100; i++){
+            
+            if(i == 35){
+                break;
+            }
+            System.out.println(i);
+        }
+        
+    }
+    
+}
+
+    
+
