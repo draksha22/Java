61fd8085e66a1e7d93377ff4c3896aadfc30b272
Draksha22 <drakshakhan2211@gmail.com>
2020-06-27 20:54:17 +0530

    this program takes input from user and will run infinitely but it will break as soon as the user enters a negative number

---
+++ Break2.java
@@ -0,0 +1,27 @@
+
+package apnikaksha;
+import java.util.Scanner;
+public class Break2 {
+    
+    public static void main(String[] args){
+        
+        
+        
+        for( ; ; ){
+            
+             Scanner sc = new Scanner(System.in);
+             System.out.println("enter a no");
+        int n = sc.nextInt();
+        
+            
+            if(n < 0){
+                
+                break;
+                
+            }
+        }
+        
+        
+    }
+    
+}
