2294255f428ab56eb1518da331ecc9a494e06843
Draksha22 <drakshakhan2211@gmail.com>
2020-06-29 20:24:11 +0530

    this programs tells that two numbers are not equal

---
+++ ComparisonOperators.java
@@ -0,0 +1,15 @@
+
+package apnikaksha;
+
+public class ComparisonOperators {
+    
+    public static void main(String[] args){
+        
+        int a = 3;
+        int b = 7;
+        
+        System.out.println(a != b);
+    }
+     
+    
+}
