90e103df2026b66c16609144fd3286dfeda08751
Draksha22 <drakshakhan2211@gmail.com>
2020-07-01 11:51:34 +0530

    this programs tells whether the no of petals are even or odd and give output accordingly

---
+++ IfElseClass.java
@@ -0,0 +1,18 @@
+
+package apnikaksha;
+
+public class IfElseClass {
+    
+    public static void main(String[] args){
+        
+        int noOfPetals = 12;
+        
+        if(noOfPetals % 2 == 0){
+            
+            System.out.println("he loves me not");
+        }else{
+            System.out.println("he loves me");
+        }
+        
+    }
+}
