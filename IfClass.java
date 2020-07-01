d798c9ff09531f6aa21cd32b9e2d302f07329669
Draksha22 <drakshakhan2211@gmail.com>
2020-07-01 11:44:43 +0530

    this is a simple if program

---
+++ IfClass.java
@@ -0,0 +1,17 @@
+
+package apnikaksha;
+
+public class IfClass {
+    
+    public static void main(String[] args){
+        
+        int age = 18;
+        
+        if (age >= 18){
+            
+            System.out.println("you can vote now");
+        }
+        
+    }
+    
+}
