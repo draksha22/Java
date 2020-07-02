de343ceb653ab5375104770107a4396167a20baf
Draksha22 <drakshakhan2211@gmail.com>
2020-07-02 13:00:04 +0530

    this program tells whether a number is greater than or less than or in between 20 and 30

---
+++ ElseIfClass.java
@@ -0,0 +1,29 @@
+
+package apnikaksha;
+
+public class ElseIfClass {
+    
+    public static void main(String[] args){
+        
+        int n = 23;
+        
+        if(n <= 0){
+            
+            System.out.println("n is less than 10");
+            
+        }else if(n > 10 && n <= 20){
+            
+            System.out.println("n is greater than 10 and less than 20");
+            
+        }else if(n > 20 && n <= 30){
+            
+            System.out.println("n is greater than 20 and less than 30");
+        
+        }else{
+        
+        System.out.println("n is greater than 30");
+    }
+        
+    }
+    
+}
