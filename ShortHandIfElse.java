0a7d7e5828f0a5cce78eb519eccbfa1c52e55030
Draksha22 <drakshakhan2211@gmail.com>
2020-07-03 12:54:00 +0530

    this is a simple program of shorthand if else

---
+++ ShortHandIfElse.java
@@ -0,0 +1,17 @@
+
+package apnikaksha;
+
+public class ShortHandIfElse {
+    
+    public static void main(String[] args){
+        
+        int a = 15;
+        int b = 8;
+        int max = 0;
+        
+        max = a > b ? a : b;
+        
+        System.out.println("max among the two no is " +max);
+        
+    }
+}
