037b4fab88d5e03fecccba08ab18a32adedff03c
Draksha22 <drakshakhan2211@gmail.com>
2020-07-04 22:31:58 +0530

    this is a simple program of for loop which prints hello 100 times

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+
+package apnikaksha;
+
+public class ForLoop {
+    
+    public static void main(String[] args){
+        
+        for(int i = 0; i < 100; i++){
+            
+            System.out.println("Hello World " +i);
+            
+        }
+        
+    }
+    
+}
