2c29b5fa6f94db722498f967839d9f6f096a947e
Draksha22 <drakshakhan2211@gmail.com>
2020-07-10 13:13:24 +0530

    this programs prints number from 1 to 100 in reverse order

---
+++ ReverseCounting.java
@@ -0,0 +1,14 @@
+
+package apnikaksha;
+
+public class ReverseCounting {
+    
+    public static void main(String[] args){
+        
+        for(int i = 100; i >0; i--){
+            
+            System.out.println(i);
+        }
+    }
+    
+}
