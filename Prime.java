a2f1ce880532135cfea0a25e9960396709567dcf
Draksha22 <drakshakhan2211@gmail.com>
2020-07-11 14:19:56 +0530

    this programs tells whether the entered number is prime or not

---
+++ Prime.java
@@ -0,0 +1,26 @@
+
+package apnikaksha;
+import java.util.Scanner;
+public class Prime {
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a no" );
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        
+        for(int i = 2; i*i < n ; i++){
+            
+            if(n % i == 0){
+                isPrime = false;
+                break;
+            }else{
+                isPrime = true;
+            }
+            
+        }System.out.println("no is Prime- " +isPrime );
+        
+    }
+    
+}
